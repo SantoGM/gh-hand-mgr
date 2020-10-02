ac2ea0ddd422e679e6d664e37aa3ee6de5c8f24d
SantoGM <suarez_s@hotmail.com>
2020-10-02 19:49:42 -0300

    Added selection mechanism

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,11 +3,20 @@ import React, { Component } from "react";
 class Cards extends Component {
   state = { isSelected: false };
 
+  handleSelectCard = () => {
+    this.props.manageHand(this.props.card.id);
+  };
+
   render() {
     const { card } = this.props;
     return (
       <div key={card.id}>
-        <img className="card-image" src={card.image} alt={card.id} />
+        <img
+          className={this.props.selected ? "card-image-selected" : "card-image"}
+          onClick={this.handleSelectCard}
+          src={card.image}
+          alt={card.id}
+        />
       </div>
     );
   }
--- src/components/Segment.js
+++ src/components/Segment.js
@@ -2,14 +2,18 @@ import React from "react";
 import Cards from "./Card";
 
 const Segment = (props) => {
-  const { cards, lvl } = props;
+  const { cards, lvl, hand, manageHand } = props;
 
   return (
     <div className="segment">
       <h3>Level {lvl == 0 ? "X" : lvl} </h3>
       {cards.map((card) => (
         <div key={card.id} className="card">
-          <Cards card={card} />
+          <Cards
+            card={card}
+            manageHand={manageHand}
+            selected={hand.includes(card.id)}
+          />
         </div>
       ))}
     </div>
