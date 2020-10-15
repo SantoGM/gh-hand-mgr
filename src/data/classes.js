997b5d9c0c5fcdbd8311d19950d6288fd647ae4d
SantoGM <suarez_s@hotmail.com>
2020-10-15 19:57:33 -0300

    Adding classes to app

--- src/data/classes.js
+++ src/data/classes.js
@@ -2,7 +2,12 @@ import MINDTHIEF from "./mindthief";
 import LIGHTNINGS from "./lightnings";
 import SUN from "./sun";
 import CTHULU from "./cthulu";
+import BRUTE from "./brute"
+import SPELLWEAVER from "./spellweaver"
+import SCOUNDREL from "./scoundrel"
+import CRAGHEART from "./cragheart"
+import TINKERER from "./tinkerer"
 
-const CLASSES = [MINDTHIEF, LIGHTNINGS, SUN, CTHULU];
+const CLASSES = [BRUTE, TINKERER, SPELLWEAVER, SCOUNDREL, CRAGHEART, MINDTHIEF, LIGHTNINGS, SUN, CTHULU];
 
 export default CLASSES;
